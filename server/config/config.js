c130ad7c6d120bcffc65e9ea953e241b409e36bb
Oscar <oma_1506@hotmail.com>
2020-04-06 01:11:07 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://admin:7uZrcyn9eRbK2E2s@cluster0-t9bqg.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
